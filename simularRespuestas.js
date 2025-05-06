b1590895ec0069e8b57bed7d867cf8a1be4e9af0
HardlinePizza21 <madridossa@gmail.com>
2025-05-05 21:38:14 -0500

    uups

--- simularRespuestas.js
+++
@@ -1,7 +0,0 @@
-const {io} = require('socket.io-client');
-require('dotenv').config();
-const socket = io('http://localhost:4000');
-
-for (let i = 0; i < 5; i++) {
-    socket.emit('sendAnswer', `Estas las respuesta placer holder numero ${i}, es solo para que se ve lindo`)
-}
